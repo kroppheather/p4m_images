a5c4047d0743fb7a1a2b85d697949068fd231309
Heather Kropp <kroppheather@gmail.com>
2021-06-09 16:36:31 -0400

    mapping

--- Data_explore.r
+++ Data_explore.r
@@ -1,6 +1,7 @@
 library(raster)
 library(tmap)
 library(mapview)
+library(sf)
 
 
 #set directory folder
@@ -87,7 +88,7 @@ stackC <- stack("e://Google Drive/GIS/drone/campus/P4M/flight_05_03_21/ortho/odm
 
 #buckthorn crop
 
-extentB <- extent(466520,466635,4767390,4767500)
+extentB <- extent(466520,466610,4767390,4767480)
 multi2c <- crop(multi2,extentB)
 stackCc <- crop(stackC, extentB)
 multi1c <- crop(multi1,extentB)
@@ -108,3 +109,66 @@ help(viewRGB)
 tm_shape(multi1c)+
   tm_rgb(r=3,g=2,b=1,
          max.value=0.13)
+
+outdir <- "E:/Google Drive/GIS/drone/campus/mapping"
+plot(stackCc[[1]])
+plotRGB(stackCc,r=3,g=2,b=1)
+
+arrows(466535,4767475,466550,4767475, code=0, lwd=4)
+text(466535,4767473, "0 m", cex=1.5)
+text(466550,4767473, "15 m", cex=1.5)
+
+arrows(466525,4767465,466525,4767473, lwd=3)
+text(466525,4767475, "N", cex=1.5)
+
+study <- st_polygon(list(rbind(c(466540.7, 4767442),
+                               c(466546.1, 4767448),
+                               c( 466543.9, 4767456),
+                               c(466558, 4767462), 
+                               c(466585.2, 4767414),
+                               c(466562.1,4767397),
+                               c(466540.7, 4767442))))
+
+st_area(study)
+
+png("e:\\Google Drive\\GIS\\drone\\campus\\mapping\\May3_map.png",
+    width=10,height=10,units="in",res=300)
+  plotRGB(stackCc,r=3,g=2,b=1, maxpixels=stackCc@ncols*stackCc@nrows)
+  
+  arrows(466535,4767400,466550,4767400, code=0, lwd=4)
+  text(466535,4767397, "0 m", cex=1.5)
+  text(466550,4767397, "15 m", cex=1.5)
+  
+  arrows(466525,4767397,466525,4767405, lwd=3)
+  text(466525,4767408, "N", cex=1.5)
+  
+  plot(study,add=TRUE, col=NA, border="#B8860B", lwd=3)
+  
+  text(466590,4767400, "study bounds ", cex=1.5, col="#B8860B")
+  text(466590,4767395, "(1450 m2)", cex=1.5,col="#B8860B")
+  
+  text(466545,4767475, "May 3, 2021", cex=2)
+
+dev.off()
+
+
+
+png("e:\\Google Drive\\GIS\\drone\\campus\\mapping\\June7_map.png",
+    width=10,height=10,units="in",res=300)
+plotRGB(multi2c,r=3,g=2,b=1, maxpixels=multi2c@ncols*multi2c@nrows)
+
+arrows(466535,4767400,466550,4767400, code=0, lwd=4)
+text(466535,4767397, "0 m", cex=1.5)
+text(466550,4767397, "15 m", cex=1.5)
+
+arrows(466525,4767397,466525,4767405, lwd=3)
+text(466525,4767408, "N", cex=1.5)
+
+plot(study,add=TRUE, col=NA, border="#B8860B", lwd=3)
+
+text(466590,4767400, "study bounds ", cex=1.5, col="#B8860B")
+text(466590,4767395, "(1450 m2)", cex=1.5,col="#B8860B")
+
+text(466545,4767475, "June 7, 2021", cex=2)
+
+dev.off()
