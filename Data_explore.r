3ec664838363eb26f9582fc7eea35773eacdf5e4
Heather Kropp <kroppheather@gmail.com>
2021-06-09 15:39:27 -0400

    looking at data and cropping

--- Data_explore.r
+++ Data_explore.r
@@ -1,5 +1,6 @@
 library(raster)
 library(tmap)
+library(mapview)
 
 
 #set directory folder
@@ -62,3 +63,48 @@ tm_shape(ndvi)+
     style="fisher",
     palette="-viridis")+
   tm_layout(legend.outside = TRUE)
+
+
+
+
+#set directory folder
+dirI3 <- "E:/Google Drive/GIS/drone/campus/P4M/flight_06_07_21/ortho/out"
+multi2 <- stack(paste0(dirI3, "/June_7_transparent_reflectance_blue.tif"),
+                paste0(dirI3, "/June_7_transparent_reflectance_green.tif"),
+                paste0(dirI3, "/June_7_transparent_reflectance_red.tif"),
+                paste0(dirI3, "/June_7_transparent_reflectance_red edge.tif"),
+                paste0(dirI3, "/June_7_transparent_reflectance_nir.tif"))
+plotRGB(multi2, r=3,g=2,b=1, scale=0.13)
+plot(multi2[[1]])
+
+
+
+
+#export images
+
+stackC <- stack("e://Google Drive/GIS/drone/campus/P4M/flight_05_03_21/ortho/odm_orthophotoRGB.tif")
+
+
+#buckthorn crop
+
+extentB <- extent(466520,466635,4767390,4767500)
+multi2c <- crop(multi2,extentB)
+stackCc <- crop(stackC, extentB)
+multi1c <- crop(multi1,extentB)
+
+plotRGB(stackCc,r=3,g=2,b=1)
+
+
+plotRGB(multi1c, r=3,g=2,b=1, scale=0.13)
+
+plot(multi2c[[1]])
+plotRGB(multi2c, r=3,g=2,b=1, scale=0.13, stretch="hist")
+multi2c@nrows*multi2c@ncols
+multi1c@nrows*multi1c@ncols
+
+viewRGB(multi2c, r=3,g=2,b=1)
+help(viewRGB)
+
+tm_shape(multi1c)+
+  tm_rgb(r=3,g=2,b=1,
+         max.value=0.13)
